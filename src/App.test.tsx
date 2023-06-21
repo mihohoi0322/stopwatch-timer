cd4a5a0ce7f7fd743fa131d05dd5eb04a5e8c027
miho <miho-miho0322@hotmail.co.jp>
2023-06-21 11:55:07 +0900

    テストの修正

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('has Start button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const startButtonElement = screen.getByText(/Start/i);
+  expect(startButtonElement).toBeInTheDocument();
 });
