6e71d55ed1c82418da77d9d4dbe2a3a606821edd
miho <miho-miho0322@hotmail.co.jp>
2023-06-21 11:45:18 +0900

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+const App: React.FC = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
+  const start = () => {
+    if (isRunning) return;
+
+    setIsRunning(true);
+    const id = setInterval(() => {
+      setTime(prevTime => prevTime + 1);
+    }, 1);
+
+    setIntervalId(id);
+  };
+
+  const pause = () => {
+    if (!isRunning) return;
+
+    setIsRunning(false);
+    if (intervalId) clearInterval(intervalId);
+  };
+
+  const clear = () => {
+    pause();
+    setTime(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [intervalId]);
+
+  const displayTime = new Date(time * 10).toISOString().substr(11, 12);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-6xl mb-4">{displayTime}</h1>
+      <div>
+        <button className="px-4 py-2 mr-2 bg-blue-500 text-white rounded" onClick={start}>Start</button>
+        <button className="px-4 py-2 mr-2 bg-yellow-500 text-white rounded" onClick={pause}>Pause</button>
+        <button className="px-4 py-2 bg-red-500 text-white rounded" onClick={clear}>Clear</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
